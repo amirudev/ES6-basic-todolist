408f1795dd8b20d9743ae05fce792683f6d452ce
wahyuamirulloh <amirullohw@gmail.com>
2020-09-30 20:58:47 +0700

    v0.4 add more button features

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 const input = document.getElementById('inputMail');
 const mail = document.getElementById('mail');
-const buttonInput = document.getElementById('buttonMail');
+const buttonStar = document.getElementById('starMail');
+const buttonLove = document.getElementById('loveMail');
+const buttonClock = document.getElementById('clockMail');
+const buttonCart = document.getElementById('cartMail');
 const liMail = document.getElementsByTagName('li');
 
 // Start create li for list
-postMail = () => {
+starMail = () => {
     var li = document.createElement('li');
-    li.appendChild(document.createTextNode(input.value));
+    var img = document.createElement('img');
+    img.src = 'star.png';
+    img.className = 'imgList';
+    var p = document.createElement('span');
+    p.textContent = input.value;
+    li.appendChild(img);
+    li.appendChild(p);
     var deleteMail = document.createElement('button');
     deleteMail.appendChild(document.createTextNode('X'));
     deleteMail.className = 'deleteButton';
     li.appendChild(deleteMail);
+    li.className += 'active';
     mail.appendChild(li);
 
     // Delete Mail function
@@ -19,6 +29,7 @@ postMail = () => {
     };
     // Strikethrough Function
     strikeMailFunction = () => {
+        li.classList.toggle('active');
         li.classList.toggle('strikethrough');
     }
 
@@ -26,4 +37,97 @@ postMail = () => {
     li.addEventListener('click', strikeMailFunction);
 }
 
-buttonInput.addEventListener('click', postMail);
+loveMail = () => {
+    var li = document.createElement('li');
+    var img = document.createElement('img');
+    img.src = 'like.png';
+    img.className = 'imgList';
+    var p = document.createElement('span');
+    p.textContent = input.value;
+    li.appendChild(img);
+    li.appendChild(p);
+    var deleteMail = document.createElement('button');
+    deleteMail.appendChild(document.createTextNode('X'));
+    deleteMail.className = 'deleteButton';
+    li.appendChild(deleteMail);
+    li.className += 'active';
+    mail.appendChild(li);
+
+    // Delete Mail function
+    deleteMailFunction = () => {
+        li.classList.add('delete');
+    };
+    // Strikethrough Function
+    strikeMailFunction = () => {
+        li.classList.toggle('active');
+        li.classList.toggle('strikethrough');
+    }
+
+    deleteMail.addEventListener('click', deleteMailFunction);
+    li.addEventListener('click', strikeMailFunction);
+}
+
+clockMail = () => {
+    var li = document.createElement('li');
+    var img = document.createElement('img');
+    img.src = 'clock1.png';
+    img.className = 'imgList';
+    var p = document.createElement('span');
+    p.textContent = input.value;
+    li.appendChild(img);
+    li.appendChild(p);
+    var deleteMail = document.createElement('button');
+    deleteMail.appendChild(document.createTextNode('X'));
+    deleteMail.className = 'deleteButton';
+    li.appendChild(deleteMail);
+    li.className += 'active';
+    mail.appendChild(li);
+
+    // Delete Mail function
+    deleteMailFunction = () => {
+        li.classList.add('delete');
+    };
+    // Strikethrough Function
+    strikeMailFunction = () => {
+        li.classList.toggle('active');
+        li.classList.toggle('strikethrough');
+    }
+
+    deleteMail.addEventListener('click', deleteMailFunction);
+    li.addEventListener('click', strikeMailFunction);
+}
+
+cartMail = () => {
+    var li = document.createElement('li');
+    var img = document.createElement('img');
+    img.src = 'shopping-cart1.png';
+    img.className = 'imgList';
+    var p = document.createElement('span');
+    p.textContent = input.value;
+    li.appendChild(img);
+    li.appendChild(p);
+    var deleteMail = document.createElement('button');
+    deleteMail.appendChild(document.createTextNode('X'));
+    deleteMail.className = 'deleteButton';
+    li.appendChild(deleteMail);
+    li.className += 'active';
+    mail.appendChild(li);
+
+    // Delete Mail function
+    deleteMailFunction = () => {
+        li.classList.add('delete');
+    };
+    // Strikethrough Function
+    strikeMailFunction = () => {
+        li.classList.toggle('active');
+        li.classList.toggle('strikethrough');
+    }
+
+    deleteMail.addEventListener('click', deleteMailFunction);
+    li.addEventListener('click', strikeMailFunction);
+}
+
+buttonStar.addEventListener('click', starMail);
+buttonLove.addEventListener('click', loveMail);
+buttonClock.addEventListener('click', clockMail);
+buttonCart.addEventListener('click', cartMail);
