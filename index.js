7bb05c69100b3b1b667f9528fb06e37ae21265af
wahyuamirulloh <amirullohw@gmail.com>
2020-09-30 13:27:46 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const input = document.getElementById('inputMail');
+const mail = document.getElementById('mail');
+const buttonInput = document.getElementById('buttonMail');
+const liMail = document.getElementsByTagName('li');
+
+// Start create li for list (001)
+postMail = () => {
+    var li = document.createElement('li');
+    li.appendChild(document.createTextNode(input.value));
+    var deleteMail = document.createElement('button');
+    deleteMail.appendChild(document.createTextNode('X'));
+    li.appendChild(deleteMail);
+    mail.appendChild(li);
+
+    // Start add .delete class
+    deleteMailFunction = () => {
+        li.classList.add('delete');
+
+        // End add .delete
+    }
+
+    deleteMail.addEventListener('click', deleteMailFunction);
+
+    // End create li (001)
+}
+
+buttonInput.addEventListener('click', postMail);
